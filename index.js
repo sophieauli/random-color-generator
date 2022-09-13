7b4c48f41c8ebc34793e78d60a4d309c6e317bf3
Sophie Aulitzky <sophie.aulitzky@gmail.com>
2022-09-13 14:39:34 +0200

    Finalize project

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import kuler from 'kuler';
 import randomColor from 'randomcolor';
 
 const color = randomColor({
-    luminosity: process.argv[3],
-    hue: process.argv[2],
+  luminosity: process.argv[3],
+  hue: process.argv[2],
 });
 // const str = kuler(, color);
 // console.log(str);
 
 console.log(
-    kuler(
-`###############################', 
+  kuler(
+    `###############################',
 ###############################
 ###############################
 #####                     #####
@@ -18,6 +18,9 @@ console.log(
 #####                     #####
 ###############################
 ###############################
-###############################`, color));
+###############################`,
+    color,
+  ),
+);
 
 // console.log(process.argv);
